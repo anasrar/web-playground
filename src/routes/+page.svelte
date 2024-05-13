f3d574a68a99dde53ff114824f6c3c694688179b
anasrar <mynameanasrar@gmail.com>
2024-05-13 23:10:21 +1200

    chore: initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	const links: { url: string; text: string }[] = [
+		{
+			url: './button-with-shine-border/',
+			text: 'Button with shine border'
+		}
+	] as const;
+</script>
+
+<svelte:head>
+	<title>Web Playground</title>
+	<meta name="description" content="Web Playground" />
+</svelte:head>
+
+<section>
+	<ul class="list-disc list-inside">
+		{#each links as link}
+			<li>
+				<a class="text-blue-700 hover:text-blue-900 underline" href={link.url}>{link.text}</a>
+			</li>
+		{/each}
+	</ul>
+</section>
